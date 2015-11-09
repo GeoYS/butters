18b6911aa47c8fa14defbb06d1d546a3cbffcafe
Michel <michelyjiang@hotmail.com>
2015-11-08 22:55:35 -0500

    Added animation that moved a imgObject using javascript... took tons of time

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
-function() 
-object.style.
+function myFunction(){ 
+	var george= document.getElementById('myImage');
+	george.style.position = 'relative';
+	george.style.left = '0px';
+	console.log("hello");
+	george.style.left = parseInt(george.style.left) + 100 + 'px';
+}
